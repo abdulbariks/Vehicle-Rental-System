c82bba41e0c98f55a789941ada9d0aee49ff2de7
abdulbariks <abdulbarik1997m@gmail.com>
2025-12-06 11:53:28 +0600

    updates

--- src/modules/users/user.routes.ts
+++ src/modules/users/user.routes.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response } from "express";
 import { userControllers } from "./user.controller";
+import auth from "../../middleware/auth.middleware";
 
 const router = express.Router();
 
-router.get("/", userControllers.getUser);
+router.get("/",auth("admin"), userControllers.getUser);
 router.get("/:id", userControllers.getSingleUser);
 
 router.put("/:id", userControllers.updateUser);
